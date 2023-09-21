768cf9688d79992a6fc2aeff4949171970d8c5f6
chiragagarwal-j <chiragagarwal.j@prodapt.com>
2023-09-21 16:46:46 +0530

    Updated Angular Based Tennis Scoreboard

---
+++ Day 17/Take Home/tennis/src/app/models/player.ts
@@ -0,0 +1,6 @@
+export class Player {
+    sets: number = 0;
+    games: number = 0;
+    score: number = 0;
+  }
+  
--- Day 17/Take Home/tennis/src/app/scoreboard/scoreboard.component.ts
+++ Day 17/Take Home/tennis/src/app/scoreboard/scoreboard.component.ts
@@ -1,95 +1,30 @@
 import { Component, HostListener } from '@angular/core';
+import { Scoreboard } from '../models/Scoreboard';
 
 
 @Component({
   selector: 'app-scoreboard',
   templateUrl: './scoreboard.component.html',
-  styleUrls: ['./scoreboard.component.css']
+  styleUrls: ['./scoreboard.component.css'],
 })
 export class ScoreboardComponent {
-  player1Sets: number = 0;
-  player2Sets: number = 0;
-  player1Games: number = 0;
-  player2Games: number = 0;
-  player1Score: number = 0;
-  player2Score: number = 0;
-
-  scoreLabels: string[] = ['Love', '15', '30', '40', 'Adv', 'Game'];
-
-  incrementPoint(player: number) {
-    if (player === 1) {
-      this.player1Score++;
-    } else {
-      this.player2Score++;
-    }
-
-    this.checkGame();
-  }
-
-  checkGame() {
-    if (this.player1Score >= 3 && this.player1Score === this.player2Score) {
-      this.player1Score = 3;
-      this.player2Score = 3;
-    } else if (this.player1Score >= 4 && this.player1Score - this.player2Score === 1) {
-      this.player1Score = 4;
-    } else if (this.player2Score >= 4 && this.player2Score - this.player1Score === 1) {
-      this.player2Score = 4;
-    } else if (this.player1Score >= 4 && this.player1Score - this.player2Score >= 2) {
-      this.player1Games++;
-      this.resetScore();
-    } else if (this.player2Score >= 4 && this.player2Score - this.player1Score >= 2) {
-      this.player2Games++;
-      this.resetScore();
-    } else if (this.player1Score === 4 && this.player2Score === 4) {
-      this.player1Score = 3;
-      this.player2Score = 3;
-    }
-
-    this.checkSet();
-  }
-
-  checkSet() {
-    if (this.player1Games >= 6 && this.player1Games - this.player2Games >= 2) {
-      this.player1Sets++;
-      this.resetGames();
-    } else if (this.player2Games >= 6 && this.player2Games - this.player1Games >= 2) {
-      this.player2Sets++;
-      this.resetGames();
-    }
-
-    if (this.player1Sets === 2) {
-      this.resetGame();
-    } else if (this.player2Sets === 2) {
-      this.resetGame();
-    }
-  }
-
-  resetScore() {
-    this.player1Score = 0;
-    this.player2Score = 0;
-  }
-
-  resetGames() {
-    this.player1Games = 0;
-    this.player2Games = 0;
-  }
-
-  resetGame() {
-    this.player1Sets = 0;
-    this.player2Sets = 0;
-    this.resetGames();
-    this.resetScore();
-  }
+  constructor(public scoreboard: Scoreboard) {}
 
   player1Scores() {
-    this.incrementPoint(1);
+    this.scoreboard.incrementPoint(1);
   }
 
   player2Scores() {
-    this.incrementPoint(2);
+    this.scoreboard.incrementPoint(2);
   }
 
-  getScoreLabel(score: number): string {
-    return this.scoreLabels[score];
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === 'ArrowUp') {
+      this.player1Scores();
+    } else if (event.key === 'ArrowDown') {
+      this.player2Scores();
+    }
   }
+  
 }
